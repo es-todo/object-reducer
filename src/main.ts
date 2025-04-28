48523304cc89a279603f81e2ac24e93bc144460d
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-04-28 11:56:39 +0300

    added api for polling change sets

--- src/main.ts
+++ src/main.ts
@@ -47,6 +47,36 @@ app.get("/object-apis/wait-t", async (req, res) => {
   return;
 });
 
+app.get("/object-apis/get-t", async (req, res) => {
+  const t = await event_monitor.get_t();
+  res.status(200).json(t);
+  return;
+});
+
+app.get("/object-apis/poll-change-set", async (req, res) => {
+  const t_str = ((t) => (typeof t === "string" ? t : ""))(req.query.t);
+  if (!t_str.match(/^\d+$/)) {
+    res.status(401).send("invalid t");
+    return;
+  }
+  const t = parseInt(t_str);
+  if (Number.isNaN(t) || t > Number.MAX_SAFE_INTEGER) {
+    res.status(401).send("invalid t");
+    return;
+  }
+  await event_monitor.wait_events(t);
+  const out = await pool.query(
+    `
+    select event_i as i, object_type as type, object_id as id, object_data as data
+    from object_rev where event_t = $1
+    order by event_i
+    `,
+    [t]
+  );
+  res.status(200).json(out.rows);
+  return;
+});
+
 app.get("/object-apis/get-object", async (req, res) => {
   const { type, id } = req.query;
   if (typeof type !== "string" || typeof id !== "string") {
@@ -54,7 +84,7 @@ app.get("/object-apis/get-object", async (req, res) => {
     return;
   }
   const out = await pool.query(
-    `select object_data as data from object_rev
+    `select event_t as t, event_i as i, object_data as data from object_rev
        where object_type = $1 and object_id = $2
        order by event_t desc, event_i desc
        limit 1`,
@@ -63,7 +93,10 @@ app.get("/object-apis/get-object", async (req, res) => {
   if (out.rows.length === 0 || out.rows[0].data === null) {
     res.status(200).json({ found: false });
   } else {
-    res.status(200).json({ found: true, data: out.rows[0].data });
+    const row = out.rows[0];
+    res
+      .status(200)
+      .json({ found: true, t: parseInt(row.t), i: row.i, data: row.data });
   }
 });
 
